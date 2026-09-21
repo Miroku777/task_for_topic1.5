8125ead5a1d5a33fe0ff2acef81607dbcf8b1854
Юра Воробьёв <yuriy_vorobev99@mail.ru>
2026-09-21 15:45:59 +0300

    task3: uppercase first letter of each word update

--- task3/task3.go
+++ task3/task3.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"os"
 	"unicode"
 )
 
@@ -10,14 +11,19 @@ import (
 // а остальные — строчными. Например: "привет мир" → "Привет Мир".
 
 func main() {
-	text := "hello world ! h h  hhhhh"
-	fmt.Println(capitalizeWords(text))
+	reader := bufio.NewReaderSize(os.Stdin, 1<<20)
+	writer := bufio.NewWriterSize(os.Stdout, 1<<20)
+	defer writer.Flush()
+	line, _ := reader.ReadString('\n')
+	writer.WriteString(capitalizeWords(line))
+	writer.WriteByte('\n')
 }
 
 func capitalizeWords(s string) string {
 	runes := []rune(s)
 	runes[0] = unicode.ToUpper(runes[0])
 	for i := 1; i < len(runes); i++ {
+		runes[i] = unicode.ToLower(runes[i])
 		if runes[i-1] == ' ' && runes[i] != ' ' {
 			runes[i] = unicode.ToUpper(runes[i])
 		}
